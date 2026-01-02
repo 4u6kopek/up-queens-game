61c33efd00a6b184c5f05695a538b1c58322952c
Bogomil Dimitrov <92969096+4u6kopek@users.noreply.github.com>
2026-01-02 23:36:26 +0200

    Initial project setup for Queens game

---
+++ Queens/Queens/game.cpp
@@ -0,0 +1,14 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author <Bogomil Dimitrov>
+* @idnumber <8MI0600665>
+* @compiler VC
+*
+* Game logic implementation
+*
+*/
---
+++ Queens/Queens/main.cpp
@@ -0,0 +1,23 @@
+﻿/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author <Bogomil Dimitrov>
+* @idnumber <8MI0600665>
+* @compiler VC
+*
+* Main entry point
+*
+*/
+
+#include <iostream>
+#include "game.h"
+
+int main() {
+	std::cout << "Queens game initialized." << std::endl;
+
+	return 0;
+}
